aae834fe218f211cd1442ef339dd210b784a2fca
TOLZ <laotech77@gmail.com>
2020-02-07 08:07:12 -0600

    Updates

---
+++ precache-manifest.0b3ec3584dd41dbea1ce44b860754a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08321e984d6c40c976bd760424ecbe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5063237ae7845fa62cac",
+    "url": "/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "4ed6cd035a10c6562c1c",
+    "url": "/static/js/2.17ed23c2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.17ed23c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5063237ae7845fa62cac",
+    "url": "/static/js/main.50d6850e.chunk.js"
+  },
+  {
+    "revision": "ef75a6ed8a6174a01bc6",
+    "url": "/static/js/runtime-main.225e90d1.js"
+  }
+]);
